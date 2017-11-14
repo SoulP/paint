abde5b3cee73ab0df7d639417b73b823486bf4d7
SoulP <info@soulp.moe>
2017-11-14 12:02:34 +0900

    継承

--- paint/src/file/test/ita/BMP_ITa.java
+++ paint/src/file/test/ita/BMP_ITa.java
@@ -12,7 +12,7 @@ import org.junit.runners.MethodSorters;
 
 import file.Tools;
 import file.io.BMP;
-import file.test.Settings;
+import file.test.Basic;
 
 /**
  * <b>BMPの内部結合テスト</b><br>
@@ -21,7 +21,7 @@ import file.test.Settings;
  * @author ソウルP
  */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class BMP_ITa extends Settings {
+public class BMP_ITa extends Basic {
     public final String addr = BASIC_ADDR + "BMP\\BMP_ITa\\";
 
     /**
@@ -37,22 +37,13 @@ public class BMP_ITa extends Settings {
         int width = 3;
         int height = 3;
 
-        // イメージ情報
-        byte[] img01 = { (byte) 0xa0, 0x00, 0x00, 0x00 };
-        byte[] img02 = { 0x40, 0x00, 0x00, 0x00 };
-        byte[] img03 = { (byte) 0xa0, 0x00, 0x00, 0x00 };
-        List<byte[]> image = new ArrayList<>();
-        image.add(img01);
-        image.add(img02);
-        image.add(img03);
-
         BMP bmp = new BMP();
         bmp.setWidth(width); // 幅
         bmp.setHeight(height); // 高さ
         bmp.setBitCount(1); // ビットの深さ
         bmp.addColor(255, 0, 255); // 紫色
         bmp.addColor(0, 255, 255); // アクア色
-        bmp.setImage(image);// イメージ
+        bmp.setImage(image_1bit);// イメージ
 
         bmp.output(file);// 出力
     }
@@ -69,13 +60,6 @@ public class BMP_ITa extends Settings {
         String file = addr + "java_4bit.bmp";
         int width = 3;
         int height = 3;
-        byte[] img01 = { 0x10, 0x10, 0x00, 0x00 };
-        byte[] img02 = { 0x01, 0x00, 0x00, 0x00 };
-        byte[] img03 = { 0x10, 0x10, 0x00, 0x00 };
-        List<byte[]> image = new ArrayList<>();
-        image.add(img01);
-        image.add(img02);
-        image.add(img03);
 
         BMP bmp = new BMP();
         bmp.setWidth(width); // 幅
@@ -83,7 +67,7 @@ public class BMP_ITa extends Settings {
         bmp.setBitCount(4); // ビットの深さ
         bmp.addColor(255, 0, 255); // 紫色
         bmp.addColor(0, 255, 255); // アクア色
-        bmp.setImage(image);// イメージ
+        bmp.setImage(image_4bit);// イメージ
 
         bmp.output(file);// 出力
     }
@@ -101,25 +85,6 @@ public class BMP_ITa extends Settings {
         String file = addr + "java_4bit_BI_RLE4.bmp";
         int width = 14;
         int height = 14;
-        // @formatter:off
-        // イメージ情報
-        byte[][] img = {
-                { 0x0e, (byte) 0xdd, 0x00, 0x00 },
-                { 0x0e, (byte) 0xcc, 0x00, 0x00 },
-                { 0x02, (byte) 0xbb, 0x0a, 0x22, 0x02, (byte) 0xbb, 0x00, 0x00 },
-                { 0x02, (byte) 0xaa, 0x01, 0x30, 0x08, (byte) 0xaa, 0x01, 0x30, 0x02, (byte) 0xaa, 0x00, 0x00 },
-                { 0x02, (byte) 0x99, 0x02, 0x49, 0x06, 0x44, 0x02, (byte) 0x94, 0x02, (byte) 0x99, 0x00, 0x00 },
-                { 0x02, (byte) 0x88, 0x03, 0x58, 0x04, (byte) 0x88, 0x03, 0x58, 0x02, (byte) 0x88, 0x00, 0x00 },
-                { 0x02, 0x77, 0x04, 0x67, 0x02, 0x66, 0x04, 0x76, 0x02, 0x77, 0x00, 0x00 },
-                { 0x02, 0x66, 0x04, (byte) 0x76, 0x02, (byte) 0x77, 0x04, 0x67, 0x02, 0x66, 0x00, 0x00 },
-                { 0x02, 0x55, 0x03, (byte) 0x85, 0x04, 0x55, 0x03, (byte) 0x85, 0x02, 0x55, 0x00, 0x00 },
-                { 0x02, 0x44, 0x02, (byte) 0x94, 0x06, (byte) 0x99, 0x02, 0x49, 0x02, 0x44, 0x00, 0x00 },
-                { 0x02, 0x33, 0x01, (byte) 0xa0, 0x08, 0x33, 0x01, (byte) 0xa0, 0x02, 0x33, 0x00, 0x00 },
-                { 0x02, 0x22, 0x0a, (byte) 0xbb, 0x02, 0x22, 0x00, 0x00 },
-                { 0x0e, 0x11, 0x00, 0x00 },
-                { 0x0e, 0x00, 0x00, 0x01 }
-        };
-        // @formatter:on
 
         BMP bmp = new BMP();
         bmp.setWidth(width); // 幅
@@ -129,7 +94,7 @@ public class BMP_ITa extends Settings {
         for (int i = 255; i >= 21; i -= 18) {
             bmp.addColor(0, i, i); // アクア色
         }
-        bmp.setImage(img);// イメージ
+        bmp.setImage(image_4bit_BI_RLE4);// イメージ
 
         bmp.output(file);// 出力
     }
@@ -147,22 +112,13 @@ public class BMP_ITa extends Settings {
         int width = 3;
         int height = 3;
 
-        // イメージ情報
-        byte[] img01 = { 0x01, 0x00, 0x01, 0x00 };
-        byte[] img02 = { 0x00, 0x01, 0x00, 0x00 };
-        byte[] img03 = { 0x01, 0x00, 0x01, 0x00 };
-        List<byte[]> image = new ArrayList<>();
-        image.add(img01);
-        image.add(img02);
-        image.add(img03);
-
         BMP bmp = new BMP();
         bmp.setWidth(width); // 幅
         bmp.setHeight(height); // 高さ
         bmp.setBitCount(8); // ビットの深さ
         bmp.addColor(255, 0, 255); // 紫色
         bmp.addColor(0, 255, 255); // アクア色
-        bmp.setImage(image);// イメージ
+        bmp.setImage(image_8bit);// イメージ
 
         bmp.output(file);// 出力
     }
@@ -180,24 +136,6 @@ public class BMP_ITa extends Settings {
         String file = addr + "java_8bit_BI_RLE8.bmp";
         int width = 13;
         int height = 13;
-        // @formatter:off
-        // イメージ情報
-        byte[][] img = {
-                { 0x0d, 0x28, 0x00, 0x00 },
-                { 0x03, 0x28, 0x0a, 0x00, 0x00, 0x00 },
-                { 0x08, 0x00, 0x05, 0x27, 0x00, 0x00 },
-                { 0x0d, 0x27, 0x00, 0x00 },
-                { 0x00, 0x0d, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x00, 0x00, 0x00 },
-                { 0x00, 0x0d, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x00, 0x00, 0x00 },
-                { 0x00, 0x0d, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x00, 0x00, 0x00 },
-                { 0x0d, 0x2a, 0x00, 0x00 },
-                { 0x0d, 0x29, 0x00, 0x00 },
-                { 0x06, 0x29, 0x01, 0x2a, 0x06, 0x29, 0x00, 0x00 },
-                { 0x0d, 0x29, 0x00, 0x00 },
-                { 0x02, 0x29, 0x01, 0x2a, 0x07, 0x29, 0x01, 0x2a, 0x02, 0x29, 0x00, 0x00 },
-                { 0x0d, 0x29, 0x00, 0x01 }
-            };
-        // @formatter:on
 
         BMP bmp = new BMP();
         bmp.setWidth(width); // 幅
@@ -213,7 +151,7 @@ public class BMP_ITa extends Settings {
         bmp.addColor(0, 255, 255); // アクア色
         bmp.addColor(0, 0, 0); // 黒色
         bmp.addColor(255, 255, 255); // 白色
-        bmp.setImage(img);// イメージ
+        bmp.setImage(image_8bit_BI_RLE8);// イメージ
 
         bmp.output(file);// 出力
     }
@@ -232,38 +170,12 @@ public class BMP_ITa extends Settings {
         String file = addr + "java_16bit.bmp";
         int width = 3;
         int height = 3;
-        // @formatter:off
-        // イメージ情報 リトルエンディアン
-        byte[] img01 = {
-                    (byte) 0xff, 0x07,
-                    0x1f, (byte) 0xf8,
-                    (byte) 0xff, 0x07,
-                    0x00, 0x00
-                };
-        byte[] img02 = {
-                    0x1f, 0x7c,
-                    (byte) 0xff, 0x07,
-                    0x1f, 0x7c,
-                    0x00, 0x00
-                };
-        byte[] img03 = {
-                    (byte) 0xff, 0x07,
-                    0x1f, (byte) 0xf8,
-                    (byte) 0xff, 0x07,
-                    0x00, 0x00
-                };
-        // @formatter:on
-
-        List<byte[]> image = new ArrayList<>();
-        image.add(img01);
-        image.add(img02);
-        image.add(img03);
 
         BMP bmp = new BMP();
         bmp.setWidth(width);// 幅
         bmp.setHeight(height); // 高さ
         bmp.setBitCount(16); // ビットの深さ
-        bmp.setImage(image); // イメージ
+        bmp.setImage(image_16bit); // イメージ
 
         bmp.output(file);// 出力
     }
@@ -283,44 +195,8 @@ public class BMP_ITa extends Settings {
         String file = addr + "java_16bit_BitFields.bmp";
         int width = 3;
         int height = 3;
-        // @formatter:off
-        // イメージ情報 リトルエンディアン
-        byte[] img01 = {
-                    (byte) 0xe0, (byte) 0x7f,
-                    (byte) 0x1f, 0x7c,
-                    (byte) 0xe0, (byte) 0x7f,
-                    0x00, 0x00
-                };
-        byte[] img02 = {
-                    (byte) 0x1f, 0x7c,
-                    (byte) 0xe0, (byte) 0x7f,
-                    (byte) 0x1f, 0x7c,
-                    0x00, 0x00
-                };
-        byte[] img03 = {
-                    (byte) 0xe0, (byte) 0x7f,
-                    (byte) 0x1f, 0x7c,
-                    (byte) 0xe0, (byte) 0x7f,
-                    0x00, 0x00
-                };
-        // @formatter:on
 
-        List<byte[]> image = new ArrayList<>();
-        image.add(img01);
-        image.add(img02);
-        image.add(img03);
-
-        // @formatter:off
-        
-        // ビットフィールドのカラーマスク ビッグエンディアン
-        byte[][] colormask = {
-                { 0x00, 0x00, 0x7c, 0x00 },         // 赤マスク
-                { 0x00, 0x00, 0x03, (byte) 0xe0 },  // 緑マスク
-                { 0x00, 0x00, 0x00, 0x1f }          // 青マスク
-        };
-        // @formatter:on
-
-        byte[] bitfields = Tools.bytes2D2bytes1D(colormask); // ビットフィールド
+        byte[] bitfields = Tools.bytes2D2bytes1D(r5g5b5Mask); // ビットフィールド
 
         BMP bmp = new BMP();
         bmp.setWidth(width);// 幅
@@ -328,7 +204,7 @@ public class BMP_ITa extends Settings {
         bmp.setBitCount(16); // ビットの深さ
         bmp.setCompression(3); // 圧縮形式
         bmp.setBitFields(bitfields); // ビットフィールド
-        bmp.setImage(image); // イメージ
+        bmp.setImage(image_16bit_BitFields); // イメージ
 
         bmp.output(file);// 出力
     }
@@ -345,38 +221,12 @@ public class BMP_ITa extends Settings {
         String file = addr + "java_24bit.bmp";
         int width = 3;
         int height = 3;
-        // @formatter:off
-        // イメージ情報
-        byte[] img01 = {
-                    (byte) 0xff, (byte) 0xff, 0x00,
-                    (byte) 0xff, 0x00, (byte) 0xff,
-                    (byte) 0xff, (byte) 0xff, 0x00,
-                    0x00, 0x00, 0x00
-                };
-        byte[] img02 = {
-                    (byte) 0xff, 0x00, (byte) 0xff,
-                    (byte) 0xff, (byte) 0xff, 0x00,
-                    (byte) 0xff, 0x00, (byte) 0xff,
-                    0x00, 0x00, 0x00
-                };
-        byte[] img03 = {
-                    (byte) 0xff, (byte) 0xff, 0x00,
-                    (byte) 0xff, 0x00, (byte) 0xff,
-                    (byte) 0xff, (byte) 0xff, 0x00,
-                    0x00, 0x00, 0x00
-                };
-        // @formatter:on
-
-        List<byte[]> image = new ArrayList<>();
-        image.add(img01);
-        image.add(img02);
-        image.add(img03);
 
         BMP bmp = new BMP();
         bmp.setWidth(width); // 幅
         bmp.setHeight(height); // 高さ
         bmp.setBitCount(24); // ビットの深さ
-        bmp.setImage(image);// イメージ
+        bmp.setImage(image_24bit);// イメージ
 
         bmp.output(file);// 出力
     }
@@ -393,35 +243,12 @@ public class BMP_ITa extends Settings {
         String file = addr + "java_32bit.bmp";
         int width = 3;
         int height = 3;
-        // @formatter:off
-        // イメージ情報
-        byte[] img01 = {
-                    (byte) 0xff, (byte) 0xff, 0x00, 0x00,
-                    (byte) 0xff, 0x00, (byte) 0xff, 0x00,
-                    (byte) 0xff, (byte) 0xff, 0x00, 0x00
-                };
-        byte[] img02 = {
-                    (byte) 0xff, 0x00, (byte) 0xff, 0x00,
-                    (byte) 0xff, (byte) 0xff, 0x00, 0x00,
-                    (byte) 0xff, 0x00, (byte) 0xff, 0x00
-                };
-        byte[] img03 = {
-                    (byte) 0xff, (byte) 0xff, 0x00, 0x00,
-                    (byte) 0xff, 0x00, (byte) 0xff, 0x00,
-                    (byte) 0xff, (byte) 0xff, 0x00, 0x00
-                };
-        // @formatter:on
-
-        List<byte[]> image = new ArrayList<>();
-        image.add(img01);
-        image.add(img02);
-        image.add(img03);
 
         BMP bmp = new BMP();
         bmp.setWidth(width); // 幅
         bmp.setHeight(height); // 高さ
         bmp.setBitCount(32); // ビットの深さ
-        bmp.setImage(image);// イメージ
+        bmp.setImage(image_32bit);// イメージ
 
         bmp.output(file);// 出力
     }
@@ -441,35 +268,8 @@ public class BMP_ITa extends Settings {
         String file = addr + "java_32bit_BitFields.bmp";
         int width = 3;
         int height = 3;
-        // @formatter:off
-        // イメージ情報
-        byte[] img01 = {
-                    0x00, 0x0f, (byte) 0xf0, (byte) 0xff,
-                    (byte) 0xff, 0x00, 0x00, (byte) 0xff,
-                    0x00, 0x0f, (byte) 0xf0, (byte) 0xff
-                };
-        byte[] img02 = {
-                    (byte) 0xff, 0x00, 0x00, (byte) 0xff,
-                    0x00, 0x0f, (byte) 0xf0, (byte) 0xff,
-                    (byte) 0xff, 0x00, 0x00, (byte) 0xff
-                };
-        byte[] img03 = {
-                    0x00, 0x0f, (byte) 0xf0, (byte) 0xff,
-                    (byte) 0xff, 0x00, 0x00, (byte) 0xff,
-                    0x00, 0x0f, (byte) 0xf0, (byte) 0xff
-                };
-        //img01 = Tools.endian(img01);
-        //img02 = Tools.endian(img02);
-        //img03 = Tools.endian(img03);
-        // @formatter:on
-
-        List<byte[]> image = new ArrayList<>();
-        image.add(img01);
-        image.add(img02);
-        image.add(img03);
 
         // @formatter:off
-        
         // ビットフィールドのカラーマスク
         /*
          * ビッグエンディアンだが、緑マスクの0x000FF000の場合、
@@ -497,7 +297,7 @@ public class BMP_ITa extends Settings {
         bmp.setBitCount(32); // ビットの深さ
         bmp.setCompression(3); // 圧縮形式
         bmp.setBitFields(bitfields); // ビットフィールド
-        bmp.setImage(image); // イメージ
+        bmp.setImage(image_32bit_BitFields); // イメージ
 
         bmp.output(file);// 出力
     }
@@ -511,7 +311,6 @@ public class BMP_ITa extends Settings {
     @Test
     public void test100_bmpInput() throws IOException {
         String file = addr + "java_4bit_BI_RLE4.bmp";
-        String comp = "";
 
         BMP bmp = new BMP();
         bmp.input(file);
@@ -522,33 +321,7 @@ public class BMP_ITa extends Settings {
         System.out.println("幅: " + bmp.getWidth());
         System.out.println("高さ: " + bmp.getHeight());
         System.out.println("ビットの深さ: " + bmp.getBitCount() + "bit");
-        switch (bmp.getCompression()) {
-            case 0:
-                comp = "BI_RGB （非圧縮）";
-                break;
-            case 1:
-                comp = "BI_RLE8 （8ビット/ピクセル）";
-                break;
-            case 2:
-                comp = "BI_RLE4 （4ビット/ピクセル）";
-                break;
-            case 3:
-                comp = "BI_BITFIELDS （ビットフィールド）";
-                break;
-            case 4:
-                comp = "BI_JPEG";
-                break;
-            case 5:
-                comp = "BI_PNG";
-                break;
-            case 6:
-                comp = "BI_ALPHABITFIELDS （アルファチャンネル付きビットフィールド）";
-                break;
-            default:
-                comp = "不明";
-                break;
-        }
-        System.out.println("圧縮形式: " + comp);
+        System.out.println("圧縮形式: " + bmp.getCompression());
         System.out.println("画像データサイズ: " + bmp.getImageSize());
         System.out.println("水平方向の解像度: " + bmp.getPixPerMeterX());
         System.out.println("垂直方向の解像度: " + bmp.getPixPerMeterY());
@@ -824,45 +597,45 @@ public class BMP_ITa extends Settings {
         // 出力
         byte[] gap1 = null;
         byte[] gap2 = null;
-            gap1 = str1.getBytes("UTF-8");
-            gap2 = str2.getBytes("UTF-8");
-            int width = 3;
-            int height = 3;
+        gap1 = str1.getBytes("UTF-8");
+        gap2 = str2.getBytes("UTF-8");
+        int width = 3;
+        int height = 3;
 
-            // イメージ情報
-            byte[] img01 = { 0x01, 0x00, 0x01, 0x00 };
-            byte[] img02 = { 0x00, 0x01, 0x00, 0x00 };
-            byte[] img03 = { 0x01, 0x00, 0x01, 0x00 };
-            List<byte[]> image = new ArrayList<>();
-            image.add(img01);
-            image.add(img02);
-            image.add(img03);
+        // イメージ情報
+        byte[] img01 = { 0x01, 0x00, 0x01, 0x00 };
+        byte[] img02 = { 0x00, 0x01, 0x00, 0x00 };
+        byte[] img03 = { 0x01, 0x00, 0x01, 0x00 };
+        List<byte[]> image = new ArrayList<>();
+        image.add(img01);
+        image.add(img02);
+        image.add(img03);
 
-            BMP bmp = new BMP(5);
-            bmp.setWidth(width); // 幅
-            bmp.setHeight(height); // 高さ
-            bmp.setBitCount(8); // ビットの深さ
-            bmp.addColor(255, 0, 255); // 紫色
-            bmp.addColor(0, 255, 255); // アクア色
-            bmp.setGap1(gap1);
-            bmp.setGap2(gap2);
-            bmp.setImage(image);// イメージ
-            bmp.output(file);// 出力
+        BMP bmp = new BMP(5);
+        bmp.setWidth(width); // 幅
+        bmp.setHeight(height); // 高さ
+        bmp.setBitCount(8); // ビットの深さ
+        bmp.addColor(255, 0, 255); // 紫色
+        bmp.addColor(0, 255, 255); // アクア色
+        bmp.setGap1(gap1);
+        bmp.setGap2(gap2);
+        bmp.setImage(image);// イメージ
+        bmp.output(file);// 出力
 
-            // 入力
-            bmp = new BMP(file);
-            String gap1Str = "";
-            String gap2Str = "";
+        // 入力
+        bmp = new BMP(file);
+        String gap1Str = "";
+        String gap2Str = "";
 
-            gap1Str = new String(bmp.getGap1(), "UTF-8");
-            gap2Str = new String(bmp.getGap2(), "UTF-8");
-            assertEquals(str1, gap1Str);
-            assertEquals(str2, gap2Str);
+        gap1Str = new String(bmp.getGap1(), "UTF-8");
+        gap2Str = new String(bmp.getGap2(), "UTF-8");
+        assertEquals(str1, gap1Str);
+        assertEquals(str2, gap2Str);
 
-            System.out.println(bmp);
-            System.out.println();
-            System.out.println("GAP1: " + gap1Str);
-            System.out.println("GAP2: " + gap2Str);
-            System.out.println();
+        System.out.println(bmp);
+        System.out.println();
+        System.out.println("GAP1: " + gap1Str);
+        System.out.println("GAP2: " + gap2Str);
+        System.out.println();
     }
 }
