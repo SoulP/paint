74fe1016fdf6399e40374bf73b3fde981ece5d6c
SoulP <info@soulp.moe>
2018-03-28 14:13:18 +0900

    テスト用の入出力先を改良とカバレッジレポート削除

--- paint/src/file/test/Settings.java
+++ paint/src/file/test/Settings.java
@@ -2,11 +2,11 @@ package file.test;
 
 /**
  * <b>設定</b><br>
- * date: 2017/10/27 last_date 2017/10/27
+ * date: 2017/10/27 last_date 2018/03/28
  * 
  * @author ソウルP
  */
 public abstract class Settings {
-    public static final String USER_NAME  = "ユーザ名";                                                // 入出力先のユーザ名
-    public static final String BASIC_ADDR = "C:\\Users\\" + USER_NAME + "\\Desktop\\Java_Paint\\"; // 入出力先
+    // 入出力先
+    public static final String BASIC_ADDR = System.getenv("userprofile") + "\\Desktop\\Java_Paint\\";
 }
